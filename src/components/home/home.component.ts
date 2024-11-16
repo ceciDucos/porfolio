76f06a220fd22893afaf19ad1782cde099ce48f3
Cecilia Ducos <cecilia.ducos@progleasing.com>
2024-11-16 15:22:39 -0300

    Responsive header and animation

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -5,24 +5,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
     standalone: true,
-    imports: [CommonModule, BrowserAnimationsModule],
+    imports: [CommonModule],
     selector: 'home',
     templateUrl: 'home.component.html',
     styleUrls: ['home.component.scss'],
     animations: [
-    trigger('fadeInSlideUp', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(20px)' }),
-        animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-      ])
-    ]),
-    trigger('fadeIn', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('800ms ease-in', style({ opacity: 1 }))
-      ])
-    ])
-  ]
+        trigger('fadeInName', [
+            transition(':enter', [
+                style({ opacity: 0 }),
+                animate('800ms ease-in', style({ opacity: 1 }))
+            ])
+        ]),
+        trigger('fadeInSlideUpTitle', [
+            transition(':enter', [
+                style({ opacity: 0, transform: 'translateY(100px)' }),
+                animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+            ])
+        ]),
+        trigger('fadeInSlideDescription', [
+            transition(':enter', [
+                style({ opacity: 0, transform: 'translateY(100px)' }),
+                animate('800ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+            ])
+        ]),
+    ],
 })
 
 export class HomeComponent implements OnInit {
